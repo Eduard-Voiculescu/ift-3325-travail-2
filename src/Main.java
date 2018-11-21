a2df47556782ef9081b807466b1eee57c98c043d
unknown <eduardvoiculescu95@gmail.com>
2018-11-21 00:19:26 -0500

    ajout projet java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        try {
+
+            System.out.println("Init 1");
+
+        } catch (Exception e) {
+
+        }
+    }
+
+}
