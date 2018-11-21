a2ec930ca228d1730aba1b56468fe8160e9f95ef
DESKTOP-SOE7HIO\Alain <alain.eid>
2018-11-21 13:15:31 -0500

    no message

---
+++ src/Trame.java
@@ -0,0 +1,18 @@
+public class Trame {
+
+    private final String flag = "01111110";
+    private String type;
+    private String num;
+    private String data;
+    private String crc;
+
+    /**
+     * Constructeur
+     */
+    public Trame() {
+    }
+
+    // TODO: constructeur
+    // TODO: getters and setters for properties
+    // TODO: checksum (crc)
+}
