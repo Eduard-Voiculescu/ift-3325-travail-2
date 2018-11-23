a3ede8829b95ce8495640cb260c2667f75610cfb
DESKTOP-SOE7HIO\Alain <alain.eid>
2018-11-22 19:40:58 -0500

    no message

--- src/TrameProcessor.java
+++ src/TrameProcessor.java
@@ -1,20 +1,20 @@
-import java.io.IOException;
 import java.net.Socket;
 
 public class TrameProcessor extends Thread {
 
     private Socket socket;
 
+    /**
+     * Constructeur
+     *
+     * @param socket Socket
+     */
     TrameProcessor(Socket socket) {
         this.socket = socket;
     }
 
     public void run() {
-        try {
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
 
 }
