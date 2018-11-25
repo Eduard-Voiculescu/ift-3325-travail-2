863b35dc3906ec35e08527d4a20f938f87591ad2
DESKTOP-SOE7HIO\Alain <alain.eid>
2018-11-24 21:04:36 -0500

    no message

--- src/TrameProcessorReceiver.java
+++ src/TrameProcessorReceiver.java
@@ -17,19 +17,85 @@ public class TrameProcessorReceiver extends Thread {
     public void run() {
 
         try {
+
             ObjectOutputStream os = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream is = new ObjectInputStream(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+
             try {
-                Trame trame = (Trame) is.readObject();
-                System.out.println(trame.makeTrameFormat() + " RECEIVED");
+
+                while (true) {
+
+                    /*
+                    Je suis pas certain du while true ici
+                    On lit plusieurs trames dans un meme thread?
+                    http://cs.lmu.edu/~ray/notes/javanetexamples/
+                     */
+
+                    Trame trame = (Trame) is.readObject();
+
+                    if (isEndOfTransmission(trame.getType())) {
+
+                        System.out.println("Receveur : Fin de la transmission");
+                        break;
+
+                    } else {
+
+                        if (hasError(trame)) {
+                            // TODO: send REJ
+                        } else {
+                            // TODO: send ACK
+                        }
+
+                    }
+
+                }
+
+
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
 
         } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
 
+            try {
+                socket.close();
+            } catch (IOException e) {
+                System.out.println("Echec de fermeture du socket");
+            }
+            
         }
-
     }
 
+    /**
+     * Checks if transmission is over by looking for type "f"
+     *
+     * @param type String
+     * @return boolean
+     */
+    private boolean isEndOfTransmission(String type) {
+        boolean isEnd = false;
+
+        CharacterConversion converter = new CharacterConversion();
+
+        if (converter.binaryToChar(type).equals("f")) isEnd = true;
+
+        return isEnd;
+    }
+
+    /**
+     * Checks if trame contains an error
+     *
+     * @param trame Trame
+     * @return boolean
+     */
+    private boolean hasError(Trame trame) {
+        boolean hasError = false;
+
+        // TODO: check for errors in trame
+
+        return hasError;
+    }
 }
